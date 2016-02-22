ace3c9181e6f408b65a1c7900e9dfd2d2be51e86
Anil Kumar Meena <anil4514@gmail.com>
2016-02-23 00:03:25 +0530

    login design and colors changed

--- app/src/main/java/thefallen/moodleplus/LoginActivity.java
+++ app/src/main/java/thefallen/moodleplus/LoginActivity.java
@@ -205,6 +205,7 @@ public class LoginActivity extends AppCompatActivity {
                         intent.putExtra("json_courses", response);
                         startActivity(intent);
                         overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+                        finish();
                     }
                     }
                 }, new Response.ErrorListener() {
